cb6c74519677a9deae070f85dba0e8f3e5f03ff1
Jake Gillenwater <JakeGill70@gmail.com>
2020-09-09 18:00:49 -0400

    Hotfix
    Duplicare classes

--- Client/ClientProgram.cs
+++ Client/ClientProgram.cs
@@ -127,25 +127,4 @@ namespace Client
         }
     }
 
-
-    public class MenuState
-    {
-        // Bad habit, but it works
-        public string _optionName; // Long name of the menu option
-        public string _interactionCode; // What the user types in the menu to select
-        public Action effect; // Function to enact
-
-        public MenuState(string name, string interactionCode, Action effect)
-        {
-            this._optionName = name;
-            this._interactionCode = interactionCode;
-            this.effect = effect;
-        }
-
-        public string getOption()
-        {
-            return _interactionCode + ". " + _optionName;
-        }
-
-        public void DoTheThing() { effect?.Invoke(); }
-    }
+}
