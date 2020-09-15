d86219506b5191d5de7f5c9522f2635541cb55ed
Jeff Roach <37549850+ArchwayEon@users.noreply.github.com>
2020-09-15 19:49:26 -0400

    Created PeerServer

---
+++ PeerToPeer/ConsoleWrapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeerToPeer
+{
+   public class ConsoleWrapper
+   {
+      private readonly object _lockObject = new object();
+
+      public void Output(int x, int y, string message)
+      {
+         lock (_lockObject)
+         {
+            Console.SetCursorPosition(x, y);
+            Console.Write(message);
+         }
+      }
+
+      public string Input(int x, int y, string prompt)
+      {
+         lock (_lockObject)
+         {
+            Console.SetCursorPosition(x, y);
+            Console.Write(prompt);
+            return Console.ReadLine();
+         }
+      }
+   }
+}
---
+++ PeerToPeer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PeerToPeer
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+         PeerServer server = new PeerServer(autoResetEvent);
+         server.Subscribe(new ServerObserver());
+
+         try
+         {
+            server.StartListening();
+            Task.Factory.StartNew(
+               () => server.WaitForConnection()
+            );
+            autoResetEvent.WaitOne();
+         }
+         catch(Exception ex)
+         {
+            Console.WriteLine($"Exception : {ex}");
+         }
+
+      }
+   }
+}
